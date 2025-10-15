b2d9fde05911991ac35ec256daf1728a487b7760
pramod <gopinath.pramod@gmail.com>
2025-10-15 17:01:21 +0530

    urls push

--- ola_backend/urls.py
+++ ola_backend/urls.py
@@ -65,8 +65,8 @@ urlpatterns = [
     path('swagger.yaml', schema_view.without_ui(cache_timeout=0), name='schema-yaml'),
     
     # API v1
-    path('api/v1/users/', include('home.urls')),
-    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('v1/users/', include('home.urls')),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     # path('api/v1/applications/', include('applications.urls')),
     # path('api/v1/products/', include('products.urls')),
     # path('api/v1/payments/', include('payments.urls')),
