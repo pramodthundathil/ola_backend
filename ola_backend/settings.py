659ee6b15c6fc39368e99649b9f3a655a48bcfa7
Dilshad <dilshadalicp28@gmail.com>
2025-10-21 15:19:16 +0530

    sender password changed

--- ola_backend/settings.py
+++ ola_backend/settings.py
@@ -245,10 +245,18 @@ MEDIA_ROOT = os.path.join(BASE_DIR,"media")
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+# EMAIL_HOST = 'smtp.gmail.com'
+# EMAIL_USE_TLS = True
+# EMAIL_PORT = 587
+# EMAIL_HOST_USER = "cepcentre007@gmail.com"
+# EMAIL_HOST_PASSWORD = 'sjcapvmtthbkdhuh'
+
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
-EMAIL_HOST_USER = "cepcentre007@gmail.com"
-EMAIL_HOST_PASSWORD = 'sjcapvmtthbkdhuh'
-
-DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+EMAIL_HOST_USER = 'profitgym.management@gmail.com'
+EMAIL_HOST_PASSWORD = 'psoovwzscskarabv'
