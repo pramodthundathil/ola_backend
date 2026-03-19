678980a24314900a87017f669bc7133a70aabf29
Pramod Gopinath <pramodgopinath@Pramods-MacBook-Air.local>
2026-03-19 12:13:25 +0530

    url change

--- ola_backend/urls.py
+++ ola_backend/urls.py
@@ -58,11 +58,11 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     
     # API Documentation
-    path('', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
-    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
-    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
-    path('swagger.json', schema_view.without_ui(cache_timeout=0), name='schema-json'),
-    path('swagger.yaml', schema_view.without_ui(cache_timeout=0), name='schema-yaml'),
+    path('api/docs/swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('api/docs/swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('api/docs/redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
+    path('api/docs/swagger.json', schema_view.without_ui(cache_timeout=0), name='schema-json'),
+    path('api/docs/swagger.yaml', schema_view.without_ui(cache_timeout=0), name='schema-yaml'),
     
     # API v1
     path('v1/users/', include('home.urls')),
