198c89b7f812ddce32f5b5862e63dcf9a17904b9
pramod <gopinath.pramod@gmail.com>
2026-07-01 10:02:40 +0530

    Refresh Git index

--- ola_backend/settings.py
+++ ola_backend/settings.py
@@ -327,6 +327,8 @@ EMAIL_USE_TLS = True
 EMAIL_PORT = 587
 EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER")
 EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD")
+# EMAIL_HOST_USER="olacreditcommunication@gmail.com"
+# EMAIL_HOST_PASSWORD="elchqarkqpudobap"
 
 
 
